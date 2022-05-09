b448ed366cd9e0d9812b13a59abf4a94d5e43ecb
Pritesh Pethani <pritesh.magespider@gmail.com>
2022-05-09 15:34:30 +0530

    new key store file generated and added into android app folder

--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+
 import 'package:pharmashots/ApiRepository/http_exception.dart';
 import 'package:pharmashots/ApiRepository/shared_pref_helper.dart';
 import 'package:pharmashots/Constants/ColorButton.dart';
@@ -162,7 +162,7 @@ class _SignInScreenState extends State<SignInScreen> {
                     GestureDetector(
                       onTap: () {
                         debugPrint("GOOGLE LOGO CLICKED");
-                        signup(context);
+                        // signup(context);
                       },
                       child: SvgPicture.asset(
                         'assets/images/icons svg/icons-google.svg',
@@ -417,23 +417,23 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  Future<void> signup(BuildContext context) async {
-    final FirebaseAuth? auth = FirebaseAuth.instance;
-    final GoogleSignIn googleSignIn = GoogleSignIn();
-    googleSignIn.signOut();
-    GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-    if (googleSignInAccount != null) {
-      final GoogleSignInAuthentication googleSignInAuthentication =
-          await googleSignInAccount.authentication;
-      final AuthCredential authCredential = GoogleAuthProvider.credential(
-          idToken: googleSignInAuthentication.idToken,
-          accessToken: googleSignInAuthentication.accessToken);
-      // Getting users credential
-      UserCredential result = await auth!.signInWithCredential(authCredential);
-      User user = result.user!;
+  // Future<void> signup(BuildContext context) async {
+  //   final FirebaseAuth? auth = FirebaseAuth.instance;
+  //   final GoogleSignIn googleSignIn = GoogleSignIn();
+  //   googleSignIn.signOut();
+  //   GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+  //   if (googleSignInAccount != null) {
+  //     final GoogleSignInAuthentication googleSignInAuthentication =
+  //         await googleSignInAccount.authentication;
+  //     final AuthCredential authCredential = GoogleAuthProvider.credential(
+  //         idToken: googleSignInAuthentication.idToken,
+  //         accessToken: googleSignInAuthentication.accessToken);
+  //     // Getting users credential
+  //     UserCredential result = await auth!.signInWithCredential(authCredential);
+  //     User user = result.user!;
 
-      debugPrint(
-          "GOOGLE_AUTH_USER-> ${user.getIdTokenResult().then((value) => debugPrint("TOKEN-->$value"))}");
-    }
-  }
+  //     debugPrint(
+  //         "GOOGLE_AUTH_USER-> ${user.getIdTokenResult().then((value) => debugPrint("TOKEN-->$value"))}");
+  //   }
+  // }
 }
